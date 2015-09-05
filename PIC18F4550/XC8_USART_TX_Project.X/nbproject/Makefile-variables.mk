3e04faebaf7c571803e07ec1232f20a2f1ffac3e
jorgevs <jevzsz@gmail.com>
2015-09-04 20:46:38 -0400

    ading nbproject folders

---
+++ PIC18F4550/XC8_USART_TX_Project.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=XC8_USART_TX_Project.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/XC8_USART_TX_Project.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xc8usarttxproject.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xc8usarttxproject.x.tar
---
+++ PIC18F4550/XC8_i2cLCD_Project.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=XC8_i2cLCD_Project.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/XC8_i2cLCD_Project.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xc8i2clcdproject.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xc8i2clcdproject.x.tar
